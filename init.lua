156ab8b5de372a9384767815a365330d2d11f9fb
CHAKKOR Bakr <bakr.chakk@gmail.com>
2024-04-12 15:02:45 +0200

    Small changes

--- init.lua
+++ init.lua
@@ -1,38 +1,8 @@
---[[
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-   what your configuration is doing, and modify it to suit your needs.
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
---]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.textwidth = 0
+vim.g.wrapmargin = 0
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
